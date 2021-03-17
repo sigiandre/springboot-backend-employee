c781d034bd0986d7777d2602c818200fc5b44e7f
Andre Diaz <andrediaz@MacBook-Pro-de-Andre.local>
2021-03-17 03:39:31 -0500

    backend commit

---
+++ src/main/java/net/javaguides/springboot/model/Employee.java
@@ -0,0 +1,62 @@
+package net.javaguides.springboot.model;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "employees")
+public class Employee {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private long id;
+	
+	@Column(name = "first_name")
+	private String FirstName;
+	
+	@Column(name = "last_name")
+	private String LastName;
+	
+	@Column(name = "email_id")
+	private String emailId;
+	
+	public Employee() {
+		
+	}
+	
+	public Employee(String firstName, String lastName, String emailId) {
+		super();
+		FirstName = firstName;
+		LastName = lastName;
+		this.emailId = emailId;
+	}
+	
+	public long getId() {
+		return id;
+	}
+	public void setId(long id) {
+		this.id = id;
+	}
+	public String getFirstName() {
+		return FirstName;
+	}
+	public void setFirstName(String firstName) {
+		FirstName = firstName;
+	}
+	public String getLastName() {
+		return LastName;
+	}
+	public void setLastName(String lastName) {
+		LastName = lastName;
+	}
+	public String getEmailId() {
+		return emailId;
+	}
+	public void setEmailId(String emailId) {
+		this.emailId = emailId;
+	}
+}
